2ecc11a64f1eacfc6c9480c457412f0363a76bd7
Atulchaurasiya-36 <atulchaurasiya66602@gmail.com>
2024-04-10 01:40:42 +0530

    Add Palindrome pattern

--- src/PalindromeTriangle.java
+++ src/PalindromeTriangle.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class PalindromeTriangle {
+public class PalindromeTriangle {
+    public static void palindrome(){
+        int k=0;
+        for(int i=1;i<=5;i++){
+            k=i;
+            for(int j=1;j<=7;j++){
+                if(j>=5-i && j<=3+i){
+                    System.out.print(k);
+                   k=(j<4)?k-1 :k+1;
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        palindrome();
+
+    }
 }
